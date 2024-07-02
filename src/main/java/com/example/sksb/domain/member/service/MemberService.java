77d83f7d7ea7bd7b8387c573cc52b04cc41bf5ae
hyunji1205 <sguswl1205@gmail.com>
2024-07-02 16:16:31 +0900

    add MemberDto

--- src/main/java/com/example/sksb/domain/member/service/MemberService.java
+++ src/main/java/com/example/sksb/domain/member/service/MemberService.java
@@ -5,6 +5,7 @@ import com.example.sksb.domain.member.entity.Member;
 import com.example.sksb.domain.member.reposirtory.MemberRepository;
 import com.example.sksb.global.exceptions.GlobalException;
 import com.example.sksb.global.rsData.RsData;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,14 +41,11 @@ public class MemberService {
     }
 
     @Getter
+    @AllArgsConstructor
     public static class AuthAndMakeTokensResponseBody {
+        private Member member;
         private String accessToken;
         private String refreshToken;
-
-        public AuthAndMakeTokensResponseBody(String accessToken, String refreshToken) {
-            this.accessToken = accessToken;
-            this.refreshToken = refreshToken;
-        }
     }
 
     @Transactional
@@ -64,7 +62,7 @@ public class MemberService {
         return RsData.of(
                 "200-1",
                 "로그인 성공",
-                new AuthAndMakeTokensResponseBody(accessToken, refreshToken)
+                new AuthAndMakeTokensResponseBody(member, accessToken, refreshToken)
         );
     }
 }
