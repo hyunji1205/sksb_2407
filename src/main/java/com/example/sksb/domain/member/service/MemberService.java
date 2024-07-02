a5f06aa7d81fdee1a3a45ca90f5f71dda30115eb
hyunji1205 <sguswl1205@gmail.com>
2024-07-02 17:02:49 +0900

    set context auth

--- src/main/java/com/example/sksb/domain/member/service/MemberService.java
+++ src/main/java/com/example/sksb/domain/member/service/MemberService.java
@@ -1,17 +1,20 @@
 package com.example.sksb.domain.member.service;
 
-
 import com.example.sksb.domain.member.entity.Member;
 import com.example.sksb.domain.member.reposirtory.MemberRepository;
 import com.example.sksb.global.exceptions.GlobalException;
 import com.example.sksb.global.rsData.RsData;
+import com.example.sksb.global.security.SecurityUser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -63,6 +66,22 @@ public class MemberService {
                 "200-1",
                 "로그인 성공",
                 new AuthAndMakeTokensResponseBody(member, accessToken, refreshToken)
+        );
+    }
+
+    public SecurityUser getUserFromAccessToken(String accessToken) {
+        Map<String, Object> payloadBody = authTokenService.getDataFrom(accessToken);
+
+        long id = (int) payloadBody.get("id");
+        String username = (String)  payloadBody.get("username");
+        List<String> authorities = (List<String>) payloadBody.get("authorities");
+
+        return new SecurityUser(
+                id,
+                username,
+                "",
+
+                authorities.stream().map(SimpleGrantedAuthority::new).toList()
         );
     }
 }
